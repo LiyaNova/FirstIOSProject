a767d8ad9f0f84f683d70977792c04f0483e99b6
Yuliya Filimonova <yuliya_nova@icloud.com>
2022-04-09 15:58:51 +0300

    InfoView changed: UIAlertController and UIAlertActions added

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -11,8 +11,30 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .blue
+        makeButton()
     }
 
+    private func makeButton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        button.center = view.center
+        button.setTitle("Back", for: .normal)
+        button.backgroundColor = .red
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
+    }
+
+    @objc private func tapAction() {
+        let alert = UIAlertController(title: "Go back", message: "Do you want to go back?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Back", style: .default) { _ in
+            self.dismiss(animated: true)
+            print("Нажата кнопка okAction")
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive) { _ in
+            print("Нажата кнопка cancelAction")
+        }
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
+    }
 }
